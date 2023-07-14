45b708aeb17e8537710a9d092c1c045e4b8ee46c
beyzanc <beyzancabuk@gmail.com>
2023-07-15 02:43:22 +0300

    files added

---
+++ README.md
@@ -0,0 +1,61 @@
+<<<<<<< HEAD
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/0mNoXTBm)
+# assignment-1
+
+
+## Appendix
+
+Person class icersindeki validation lar FluentValidation kutuphanesi kullanilarak yeniden duzenlenecek.
+Controller uzerindeki POST methodu attributelar yerinde FluentValidation ile calisacak sekilde duzenlenecek. 
+Odev icerisinde sadece 1 controller ve 1 method teslim edilecek. 
+
+
+
+```c#=
+public class Person
+{
+    [DisplayName("Staff person name")]
+    [Required]
+    [StringLength(maximumLength: 100, MinimumLength = 5)]
+    public string Name { get; set; }
+
+
+    [DisplayName("Staff person lastname")]
+    [Required]
+    [StringLength(maximumLength: 100, MinimumLength = 5)]
+    public string Lastname { get; set; }
+
+
+    [DisplayName("Staff person phone number")]
+    [Required]
+    [Phone]
+    public string Phone { get; set; }
+
+
+    [DisplayName("Staff person access level to system")]
+    [Range(minimum: 1, maximum: 5)]
+    [Required]
+    public int AccessLevel { get; set; }
+
+
+
+    [DisplayName("Staff person salary")]
+    [Required]
+    [Range(minimum: 5000, maximum: 50000)]
+    [SalaryAttribute]
+    public decimal Salary { get; set; }
+}
+```
+
+
+## Badges
+
+Add badges from somewhere like: [shields.io](https://shields.io/)
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
+=======
+# fluent-validation-demo
+>>>>>>> f2b0c0f (files added)
---
+++ Sipay.Bootcamp.Beyza-Cabuk.FluentValidationDemo/Validators/PersonValidator.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using Sipay.Bootcamp.Beyza_Cabuk.FluentValidationDemo.Models;
+using System.Text.RegularExpressions;
+
+
+namespace Sipay.Bootcamp.Beyza_Cabuk.FluentValidationDemo.Validators
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("Staff person name is required.")
+                .Length(5, 100).WithMessage("The name must be between 5 and 100 characters.");
+
+            RuleFor(p => p.Lastname)
+                .NotEmpty().WithMessage("Staff person lastname is required.")
+                .Length(5, 100).WithMessage("The lastname must be between 5 and 100 characters.");
+
+            RuleFor(p => p.Phone)
+                .NotEmpty().WithMessage("Staff person phone number is required.")
+                .Must(IsPhoneNumber).WithMessage("The phone number is invalid.");
+
+            RuleFor(p => p.AccessLevel)
+                .NotEmpty().WithMessage("Staff person access level is required.")
+                .InclusiveBetween(1, 5).WithMessage("Staff person access level must be between 1 and 5.");
+
+            RuleFor(p => p.Salary)
+                .NotEmpty().WithMessage("Staff person salary is required")
+                .InclusiveBetween(5000, 50000).WithMessage("Staff person salary must be between 5000 and 50.000.")
+                .Must((person, salary) => IsValidSalary(person.AccessLevel, salary)).WithMessage("Salary is not valid for the given access level.");
+        }
+
+        private bool IsPhoneNumber(string phoneNumber)
+        {
+            Regex regex = new Regex(@"^[0-9]{10}$");
+            return regex.IsMatch(phoneNumber);
+        }
+
+        private bool IsValidSalary(int accessLevel, decimal salary)
+        {
+            switch (accessLevel)
+            {
+                case 1:
+                    return salary <= 10000;
+                case 2:
+                    return salary <= 20000;
+                case 3:
+                    return salary <= 30000;
+                case 4:
+                    return salary <= 40000;
+                default:
+                    return false;
+            }
+        }
+    }
+}
